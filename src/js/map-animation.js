61a644ff50d5624aee7fdcc94a36c5cfa8dc0755
melnik <melnik@bifit.com>
2022-06-07 18:55:50 +0300

    fix

---
+++ src/js/map-animation.js
@@ -0,0 +1,55 @@
+const TIMER_DELAY = 500;
+const btnStart = document.querySelector('.map-black__start');
+const btnRepeat = document.querySelector('.map-black__repeat');
+
+btnStart.addEventListener('click', btnStartClick);
+
+function btnStartClick() {
+  const countries = getCountriesData();
+  [].concat(...Object.values(countries)).forEach(el => el.classList.remove('active'));
+  showCountriesLight(Object.values(countries), TIMER_DELAY);
+}
+
+/**
+ * render-функция анимации с заданной задержкой
+ * @param elements массив элементов анимации
+ * @param delay задержка
+ */
+function showCountriesLight(elements, delay) {
+  let i = 0;
+  const activeClassName = 'active';
+
+  function startLight() {
+    if (i === elements.length) {
+      return;
+    }
+    elements[i].forEach(el => el.classList.add(activeClassName));
+    i++;
+    setTimeout(startLight, delay);
+  }
+
+  setTimeout(startLight, delay);
+}
+
+/**
+ * Сформировать объект хранения всех элементов анимации
+ * @returns Объект html элементов, где свойство - название страны, значение - массив элементов этой страны
+ */
+function getCountriesData() {
+  const countryElements = Array.from(document.querySelectorAll('path.country'));
+
+  return countryElements.reduce((result, current) => {
+    const name = current.dataset.country;
+    result[name] = result[name] ? [...result[name], current] : [current];
+    return result;
+  }, {})
+}
+
+function removeClass(styleClass, elements) {
+
+}
+
+
+
+
+
