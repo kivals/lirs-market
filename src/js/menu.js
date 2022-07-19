2f025a3ba081e174a96c882f0b6cfd71333f4694
melnik <melnik@bifit.com>
2022-07-19 12:14:48 +0300

    Set active menu link

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,7 +1,20 @@
 const menuBurger = document.querySelector(".burger");
 const menuBody = document.querySelector(".menu__body");
 
-menuBurger.addEventListener("click", (e) => {
+const switchActiveMenu = () => {
   menuBurger.classList.toggle("_active");
   menuBody.classList.toggle("_active");
+}
+
+menuBody.addEventListener("click", switchActiveMenu);
+
+menuBurger.addEventListener("click", switchActiveMenu);
+
+
+const routeLinks = document.querySelectorAll('.nav-list__link');
+Array.from(routeLinks).forEach(link => {
+  const linkUrl = new URL(link.href);
+  if (linkUrl.pathname === document.location.pathname) {
+    link.classList.add('_active');
+  }
 })
